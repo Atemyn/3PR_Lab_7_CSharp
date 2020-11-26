49fe98f723afd18922f57d19c68e92076d7b0e8d
Atemyn <atemyn@mail.ru>
2020-11-26 14:54:16 +0300

    Добавьте файлы проекта.

---
+++ 3PR_Lab_7_CSharp/Facade.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _3PR_Lab_7_СSharp
+{
+	class Facade
+	{
+		private int windowsAmount;
+		private int openedWindowsAmount;
+		public int WindowsAmount
+		{
+			get
+			{
+				return windowsAmount;
+			}
+			set
+			{
+				windowsAmount = value;
+			}
+		}
+		public int OpenedWindowsAmount
+		{
+			get
+			{
+				return openedWindowsAmount;
+			}
+			set
+			{
+				openedWindowsAmount = value;
+			}
+		}
+		// Функция по заданию приватных полей класса.
+		public void setFacade(int windowsAmount, int openedWindowsAmount)
+		{
+			this.windowsAmount = windowsAmount;
+			this.openedWindowsAmount = openedWindowsAmount;
+		}
+		// Функция, выводящая приватные поля в консоль.
+		public void getFacade()
+		{
+			Console.WriteLine("Общее количество окон: " + windowsAmount);
+			Console.WriteLine("Количество открытых окон: " + openedWindowsAmount);
+		}
+		// Функция по вводу полей.
+		public void inputFacade()
+		{
+			Console.Write("Введите общее количество окон вашего здания: ");
+			while (!(int.TryParse(Console.ReadLine(), out windowsAmount)) || windowsAmount < 0)
+			{
+				Console.Write("Неверный ввод количества окон - оно должно быть целым неотрицательным числом. Попробуйте еще раз: ");
+			}
+
+			Console.Write("Введите количество открытых окон вашего здания: ");
+			while (!(int.TryParse(Console.ReadLine(), out openedWindowsAmount)) || openedWindowsAmount < 0 || openedWindowsAmount > windowsAmount)
+			{
+				Console.Write("Неверный ввод количества открытых окон - оно должно быть не меньше нуля и не больше общего числа окон. Попробуйте еще раз: ");
+			}
+		}
+		// Функция по открытию определенного числа окон.
+		public void openWindows()
+		{
+			int windowsToOpen;
+			Console.Write("Введите количество окон, которые вы хотите открыть: ");
+			while (!(int.TryParse(Console.ReadLine(), out windowsToOpen)) || windowsToOpen < 0 || windowsToOpen > (windowsAmount - openedWindowsAmount))
+			{
+				Console.Write("Неверный ввод количества окон для открытия - оно должно быть не меньше нуля и не больше возможного для открытия числа окон. Попробуйте еще раз: ");
+			}
+
+			openedWindowsAmount += windowsToOpen;
+		}
+		// Функция по закрытию определенного числа окон.
+		public void closeWindows()
+		{
+			int windowsToClose;
+			Console.Write("Введите количество окон, которые вы хотите закрыть: ");
+			while (!(int.TryParse(Console.ReadLine(), out windowsToClose)) || windowsToClose < 0 || windowsToClose > openedWindowsAmount)
+			{
+				Console.Write("Неверный ввод количества окон для закрытия - оно должно быть не меньше нуля и не больше числа открытых окон. Попробуйте еще раз: ");
+			}
+
+			openedWindowsAmount -= windowsToClose;
+		}
+		// Функция по сложению двух объектов класса Façade.
+		public void addToFacade(Facade facadeToAdd)
+		{
+			windowsAmount += facadeToAdd.windowsAmount;
+			openedWindowsAmount += facadeToAdd.openedWindowsAmount;
+		}
+	};
+}
+
---
+++ 3PR_Lab_7_CSharp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3PR_Lab_7_СSharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/* Работа с экземплярами класса. */
+			Building firstBuilding = new Building();
+			Building secondBuilding = new Building();
+
+			Console.WriteLine("Программа по созданию зданий.\nВсе размерные данные вводятся в метрах.\n");
+			Console.WriteLine("Работа с экземплярами класса: ");
+
+			firstBuilding.inputBuilding();
+			secondBuilding.initBuilding();
+
+			firstBuilding.addToBuilding(secondBuilding);
+
+			firstBuilding.addFloors();
+			firstBuilding.removeFloors();
+			/*............................................*/
+
+			/* Список объектов. */
+			List<Building> firstDynamicArray = new List<Building>();
+			Building helpBuilding = new Building();
+
+			Console.WriteLine("\nСписок объектов: ");
+
+			firstDynamicArray.Add(new Building());
+			firstDynamicArray[0].inputBuilding();
+			firstDynamicArray.Add(new Building());
+			firstDynamicArray[1].initBuilding();
+			firstDynamicArray[0].addToBuilding(firstDynamicArray[1]);
+			/*...............................*/
+
+			const int amount = 2;
+			/* Массив экземпляров. */
+			Building[] firstArrayOfDyn = new Building[amount];
+			Building[] secondArrayOfDyn = new Building[amount];
+			for (int i = 0; i < amount; i++)
+			{
+				firstArrayOfDyn[i] = new Building();
+				secondArrayOfDyn[i] = new Building();
+			}
+
+			Console.WriteLine("\nМассив объектов: ");
+
+			firstArrayOfDyn[0].inputBuilding();
+			secondArrayOfDyn[1].initBuilding();
+
+			firstArrayOfDyn[0].addToBuilding(secondArrayOfDyn[1]);
+			/*..............................*/
+
+			/* Новые функции Ассоциации. */
+			Building building = new Building();
+			Console.WriteLine("\nНовые функции Ассоциации: ");
+
+			building.inputBuilding();
+
+			building.openWindowsOnFacade();
+			building.closeWindowsOnFacade();
+			/*..........................*/
+		}
+	}
+}
